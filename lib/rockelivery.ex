165aa3e31f40900838c054dd2b5a8da915c46bc4
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2022-04-17 09:08:00 -0300

    Update rockelivery.ex

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,7 +1,7 @@
 defmodule Rockelivery do
   
   # When the application receive a call for create_user(), it will 
-  # delegate the call for UserCreate (/lib/rockelivery/users/create.ex), 
+  # delegate the call to UserCreate (/lib/rockelivery/users/create.ex), 
   # calling call() function
   alias Rockelivery.Users.Create, as: UserCreate
   defdelegate create_user(params), to: UserCreate, as: :call
